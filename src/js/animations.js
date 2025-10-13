9143989228575aaea9816a4ffb4bd8da16994231
sergey_panteleenko <depstor@vk.com>
2025-10-14 00:02:05 +0300

    header-mobile-v2

--- src/js/animations.js
+++ src/js/animations.js
@@ -7,6 +7,26 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
+    const searcShow = () => {
+        try {
+            const btn = document.querySelector('#search-box');
+            const inputSearch = document.querySelector('input[data-search]');
+            const logo = document.querySelector('.header__logo');
+            btn.addEventListener('click', () => {
+                inputSearch.classList.toggle('header__search-input--active')
+                if (!inputSearch.classList.contains('.header__search-input--active')) {
+                    inputSearch.value = ''
+                    logo.classList.toggle('header__logo--show')
+                }
+            })
+        } catch (error) {
+
+        }
+
+
+    }
+
+    searcShow()
     isToggle('header__hamburger--active', 'hamburger')
 
 });
